aaecb39945b9c1ea9f94797ebac0fc436fee1d6c
Alexey Egoshin <92209523+RipeCherries@users.noreply.github.com>
2026-09-25 13:46:22 +0700

    Delete backend/migrate2.sql

--- backend/migrate2.sql
+++
@@ -1,6 +0,0 @@
--- Шаг 1: навешиваем FK appointments -> staff
-ALTER TABLE appointments ADD CONSTRAINT fk_appointments_staff
-    FOREIGN KEY (doctor_id) REFERENCES staff(id) ON DELETE SET NULL;
-
--- Шаг 2: убираем старую таблицу doctors
-DROP TABLE IF EXISTS doctors;
